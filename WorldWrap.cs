0a8573b20050481e35c31f25d5ec3a76529acdc8
matt-mekha <mtmekha8@gmail.com>
2020-05-21 11:37:21 -0500

    init

---
+++ WorldWrap.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace WorldWrap
+{
+	public class WorldWrap : Mod
+	{
+		
+	}
+}
---
+++ WorldWrapPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WorldWrap
+{
+	public class WorldWrapPlayer : ModPlayer
+	{
+
+		private const float worldEdge = 800;
+
+		public override void PostUpdate()
+		{
+			float x = player.position.X;
+			if (x <= worldEdge)
+			{
+				x += Main.rightWorld - 2 * worldEdge;
+			}
+			else if (x >= Main.rightWorld - worldEdge)
+			{
+				x += -Main.rightWorld + 2 * worldEdge;
+			}
+			player.position.X = x;
+		}
+	}
+}
+
